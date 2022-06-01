0fdb43a891e6947a79c33adf8e2108b7e41b78b4
Velico <72202252+pedrojorquera@users.noreply.github.com>
2022-06-01 13:30:39 +0200

    Documentacion y traduccion al español

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,17 +2,17 @@ import { config } from 'dotenv';
 config();
 
 export default {
-    // App config
+    // Configuracion de puerto
     PORT: process.env.PORT || 4000,
 
-    // Database
+    // Base de datos
     MONGODB_URI: process.env.MONGODB_URI || undefined,
     
-    // Secret keys
+    // Claves secretas
     EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET || 'secret',
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
 
-    // Admin user credentials
+    // Credenciales de usuario administrador
     ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'admin@localhost.com',
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
     ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
